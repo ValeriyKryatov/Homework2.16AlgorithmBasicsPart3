d180c1af453b5751d1a3532ff7da6f698e799954
ValeriyKryatov <Kwaler@mail.ru>
2023-09-21 22:58:18 +0300

    Homework2.16AlgorithmBasicsPart3. ДЗ сделано.

---
+++ src/itemNotFoundException.java
@@ -0,0 +1,20 @@
+public class itemNotFoundException extends RuntimeException {
+    public itemNotFoundException() {
+    }
+
+    public itemNotFoundException(String message) {
+        super(message);
+    }
+
+    public itemNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public itemNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public itemNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
